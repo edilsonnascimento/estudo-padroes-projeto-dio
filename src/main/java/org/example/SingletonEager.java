db66c7a6de520eef9480495bbb4598834e703ffc
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-23 11:12:13 -0300

    03 -ADD Single Eager

---
+++ src/main/java/org/example/SingletonEager.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SingletonEager {
+    private final static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstance() {
+        return instancia;
+    }
+}
