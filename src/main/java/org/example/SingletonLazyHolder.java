0f43bf5a726e11ea095663dee15381c548e3c233
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-23 11:16:50 -0300

    04 -ADD Lazy Holder

---
+++ src/main/java/org/example/SingletonLazyHolder.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class SingletonLazyHolder {
+
+    public static class InstanceHolder {
+        private final static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstance() {
+        return InstanceHolder.instancia;
+    }
+}
